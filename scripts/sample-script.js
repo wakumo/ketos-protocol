b5994cb7a290d725ac74f17fc46bef29f94747a5
longhoang.wkm <longhoang@wakumo.vn>
2021-12-31 08:29:31 +0700

    chore: update gitignore

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -3,17 +3,42 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat')
 
 async function main() {
-  [treasury, borrower, lender, ...addrs] = await ethers.getSigners();
+  [treasury, ...addrs] = await ethers.getSigners()
 
-  const PawnShop = await hre.ethers.getContractFactory("PawnShop");
-  const pawnShop = await PawnShop.deploy(treasury);
+  const PawnShop = await hre.ethers.getContractFactory('PawnShop')
+  const pawnShop = await PawnShop.deploy(treasury.address, treasury.address)
 
-  await pawnShop.deployed();
+  console.log("=======================================================")
+  console.log("====...................DEPLOYING.......................")
+  console.log("====...................................................")
+  console.log("=======================================================")
+  await pawnShop.deployed()
+  console.log("RECEIPT")
+  console.log(await pawnShop.deployTransaction.wait())
+  console.log("DEPLOYED!")
+  console.log('PawnShop deployed to:', pawnShop.address)
 
-  console.log("PawnShop deployed to:", pawnShop.address);
+  console.log("\n\n=======================================================")
+  console.log("====...................SET SERVICE RATE................")
+  console.log("====...................................................")
+  console.log("=======================================================")
+  await pawnShop.setServiceFeeRates(
+    [
+      '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+    ],
+    [0],
+  )
+  console.log("SET!")
+
+  // check service fee rate
+  console.log("\n\nRECHECK service fee rate")
+  console.log(`Service fee ETH: ${await pawnShop.getServiceFeeRate('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')}`)
+  console.log(`ETH supported: ${await pawnShop.supportedTokens('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')}`)
+
+  console.log("\nFINISH!!!")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -21,6 +46,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
